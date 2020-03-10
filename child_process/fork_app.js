b964687c3402cb82438f93e5d6ae32774994b45c
Chris Song <1352411860@qq.com>
2020-03-10 13:27:16 +0800

    add  node进程

---
+++ child_process/fork_app.js
@@ -0,0 +1,28 @@
+const fork = require('child_process').fork;
+const http = require('http')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    debugger
+    if(req.url == '/compute'){
+        const compute = fork( path.join(__dirname,'./fork_compute.js') );
+        compute.send('开启一个新的子进程');
+
+        // 当一个子进程使用 process.send() 发送消息时会触发 'message' 事件
+        compute.on('message', sum => {
+            res.end(`Sum is ${sum}`);
+            compute.kill();
+        });
+
+        // 子进程监听到一些错误消息退出
+        compute.on('close', (code, signal) => {
+            console.log(`收到close事件，子进程收到信号 ${signal} 而终止，退出码 ${code}`);
+            compute.kill();
+        })
+    }else{
+        res.end(`ok`);
+    }
+});
+server.listen(3000, '127.0.0.1', () => {
+    console.log(`server started at http://${'127.0.0.1'}:${3000}`);
+});
---
+++ cluster/index.js
@@ -0,0 +1,27 @@
+// 单个 Node.js 实例运行在单个线程中。 为了充分利用多核系统，有时需要启用一组 Node.js 进程去处理负载任务。
+
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`主进程 ${process.pid} 正在运行`);
+
+  // 衍生工作进程。
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`工作进程 ${worker.process.pid} 已退出`);
+  });
+} else {
+  // 工作进程可以共享任何 TCP 连接。
+  // 在本例子中，共享的是 HTTP 服务器。
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('你好世界\n');
+  }).listen(8000);
+
+  console.log(`工作进程 ${process.pid} 已启动`);
+}
